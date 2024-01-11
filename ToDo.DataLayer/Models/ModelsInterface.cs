91efd8fbfe05f36a3aadbe2ecf125f8166727424
AliSabet <alisabasus@gmail.com>
2024-01-11 05:55:53 +0330

    interface for using in modify method in repository calss aded in model folder

---
+++ ToDo.DataLayer/Models/ModelsInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.DataLayer.Models
+{
+    public interface ModelsInterface
+    {
+    }
+}
