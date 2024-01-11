2c7493470c0a985211d1878ec3b7abe2e3b4c7a3
AliSabet <alisabasus@gmail.com>
2024-01-11 06:00:02 +0330

    i repository interface added

--- ToDo.DataLayer/Models/TaskStatusModel.cs
+++ ToDo.DataLayer/Models/TaskStatusModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDo.DataLayer.Models;
 
 namespace ToDoApp.Models
 {
-    public class TaskStatusModel
+    public class TaskStatusModel : ModelsInterface
     {
         public int TaskId { get; set; }
         public int TaskStatus { get; set; }
--- ToDo.DataLayer/Repositories/IRepository.cs
+++ ToDo.DataLayer/Repositories/IRepository.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
+using ToDo.DataLayer;
+using ToDo.DataLayer.Models;
 
 namespace ToDo.DataLayer.Repositories
 {
-    interface IRepository
+    public interface IRepository
     {
+        DataTable GetTable();
+        DataRow GetById(int id);
+        bool Add(ModelsInterface model);
+        bool Delete(int id);
+        bool Modify(int id, ModelsInterface model);
+
     }
 }
