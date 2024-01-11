608d24ede80fc8c9204bd78703ccd6db60807dac
AliSabet <alisabasus@gmail.com>
2024-01-11 05:47:24 +0330

    model added to the data layer

---
+++ ToDo.DataLayer/Models/ProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ProfileModel
+    {
+        public int Id { get; set; }
+        public string ProfileName { get; set; }
+    }
+}
