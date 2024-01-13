702d488e61f0d8d451d0b194e46abf8bf05270a6
AliSabet <alisabasus@gmail.com>
2024-01-13 22:17:35 +0330

    periodity model added

---
+++ ToDo.DataLayer/Models/PeriodityModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.DataLayer.Models
+{
+    public class PeriodityModel
+    {
+        public int Id { get; set; }
+        public string PeriodityName { get; set; }
+    }
+}
